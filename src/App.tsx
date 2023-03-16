01f882987b665ee335c85c4043f57dbd66fd2e0f
codingisfun <61808280+jennyq7@users.noreply.github.com>
2023-03-16 19:42:04 -0400

    set up html for calculator and type

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,45 @@ import React from 'react';
 import './App.scss';
 
 function App() {
+
+  const createDigits = () => {
+    let digits : React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>[] = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button key={i}>{i}</button>
+      )
+    }
+    return digits;
+  }
+
   return (
     <div className="App">
-      <p>test</p>
-    </div>
+      <div className="calculator">
+
+        <div className="display">
+          <span className="display__span">(0)</span> 0
+        </div>
+
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+
+          <button>DEL</button>
+        </div>
+
+        <div className="digits">
+          <>{ createDigits() }</>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+
+
+
+      </div>
+    </div> 
   );
 }
 
